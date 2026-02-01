0c6d24ded86407e4ad6d6957e56a384f6cfdf9e9
Clawd <t.800.y.1984@gmail.com>
2026-02-01 15:38:18 +0900

    feat: deploy SwapRouter + update address
    Router: 0xea24595d46620f4D7398217eaA603c3Df2902F88

--- script/DeployRouter.s.sol
+++ script/DeployRouter.s.sol
@@ -1,21 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-import {Script, console2} from "forge-std/Script.sol";
+import "forge-std/Script.sol";
 import {PumpClawSwapRouter} from "../src/helpers/PumpClawSwapRouter.sol";
 
-contract DeployRouterScript is Script {
+contract DeployRouter is Script {
+    // Base mainnet addresses
     address constant POOL_MANAGER = 0x498581fF718922c3f8e6A244956aF099B2652b2b;
     address constant WETH = 0x4200000000000000000000000000000000000006;
-    
+
     function run() external {
-        uint256 deployerPrivateKey = vm.envUint("BASE_PRIVATE_KEY");
-        
-        vm.startBroadcast(deployerPrivateKey);
-        
+        vm.startBroadcast();
+
         PumpClawSwapRouter router = new PumpClawSwapRouter(POOL_MANAGER, WETH);
         console2.log("PumpClawSwapRouter deployed at:", address(router));
-        
+
         vm.stopBroadcast();
     }
 }
