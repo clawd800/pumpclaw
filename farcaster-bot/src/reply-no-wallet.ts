164c6fe325a6ff73a88cab687a3d68cfa8eda3e9
Clawd <t.800.y.1984@gmail.com>
2026-02-27 20:22:08 +0900

    chore: untrack farcaster-bot (internal ops); remove outdated files (tools, docs, scripts)

--- farcaster-bot/src/reply-no-wallet.ts
+++
@@ -1,5 +0,0 @@
-import { replyCast } from './farcaster.js';
-const hash = process.argv[2];
-const author = process.argv[3];
-const msg = `@${author} Hey! To deploy your token, you need a verified ETH address on your Farcaster profile. Add one at warpcast.com/~/settings and then mention me again! 🦞`;
-replyCast(hash, msg).then(h => { console.log('replied:', h); process.exit(0); }).catch(e => { console.error('error:', e.message); process.exit(1); });
